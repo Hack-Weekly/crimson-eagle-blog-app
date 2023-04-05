0cff75c4ad60efd7160d752f2bd8fbd19df677d8
Sanyam Jain <sanyamjainsj04@gmail.com>
2023-04-05 17:55:04 +0530

    improved code

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+export const Login = ({
+  onFormSwitch,
+}: {
+  onFormSwitch: (type: "login" | "register") => void;
+}) => {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(email);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center border-2 border-primary p-20 font-publicsans">
+      <form className="flex flex-col items-center" onSubmit={handleSubmit}>
+        <label className="px-0 py-1 text-left text-primary" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="mx-0 my-2 rounded border-2 border-primary p-4"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="youremail@email.com"
+          id="email"
+          name="email"
+        />
+        <label className="px-0 py-1 text-left text-primary" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="mx-0 my-2 rounded border-2 border-primary p-4"
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+          type="password"
+          placeholder="********"
+          id="password"
+          name="password"
+        />
+        <button className="p-1.25 cursor-pointer rounded-sm border-none border-primary bg-secondary bg-none px-20 py-1">
+          Log In
+        </button>
+      </form>
+      <button
+        className="cursor-pointer bg-none py-2 text-primary underline"
+        onClick={() => onFormSwitch("register")}
+      >
+        Don&apos;t have an account? Register here.
+      </button>
+    </div>
+  );
+};
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,37 +1,43 @@
 import React, { useState } from "react";
 import { type NextPage } from "next";
 import Head from "next/head";
-import { Login } from "./Login"
-import { Register } from "./Register"
+import { Login } from "./Login";
+import { Register } from "./Register";
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
         <title>Crimson Blogs</title>
-        <meta name="description" content="A blog Apllication where you can write blogs." />
+        <meta
+          name="description"
+          content="A blog Apllication where you can write blogs."
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <main className="flex min-h-screen w-full flex-col items-center justify-center bg-primary">
+        <App />
       </main>
     </>
   );
 };
 
 function App() {
-  const [currentForm,setCurrentForm] = useState('login');
+  const [currentForm, setCurrentForm] = useState("login");
 
-  const toggleForm = (formName) => {
-    setCurrentForm(formName)
-  }
+  const toggleForm = (type: "login" | "register") => {
+    setCurrentForm(type);
+  };
 
   return (
-    <div className="text-center flex min-h-screen items-center justify-center bg-primary" >
-      {
-        currentForm == "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-      }
+    <div className="flex min-h-screen items-center justify-center text-center">
+      {currentForm == "login" ? (
+        <Login onFormSwitch={toggleForm} />
+      ) : (
+        <Register onFormSwitch={toggleForm} />
+      )}
     </div>
-  )
+  );
 }
 
-export default App;
+export default Home;
